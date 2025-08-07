6577487cd1e806950adef1d3bddc76797a323152
Armin <bigboss@diamond.dogs>
2025-08-07 13:42:10 +0330

    added badges

--- src/atomic-design/organisms/UserList/index.tsx
+++ src/atomic-design/organisms/UserList/index.tsx
@@ -25,6 +25,27 @@ const UserList = () => {
     };
   }, []);
 
+  let score_1 = 0, score_2 = 0, score_3 = 0;
+  [score_1, score_2, score_3] = [...new Set(users.map((i: User) => i.active_test_score))] // حذف تکرارها
+  // @ts-ignore
+  .sort((a, b) => b - a) // مرتب کردن به ترتیب نزولی
+  .slice(0, 3) // گرفتن ۳ عدد بزرگتر
+  // @ts-ignore
+  .map((score: number) => score || 0); // اطمینان از اینکه اگر مقدار undefined باشد، صفر قرار گیرد
+
+  const showBadge = (result: number) => {
+    switch (result) {
+      case score_1:
+        return <img src="../../assets/img/badge/1.png" style={{width: '1.7em'}} />
+      case score_2:
+        return <img src="../../assets/img/badge/2.png" style={{width: '1.7em'}} />
+      case score_3:
+        return <img src="../../assets/img/badge/3.png" style={{width: '1.7em'}} />
+      default:
+        return <i className="bx bx-award h3 mb-0"></i>
+    }
+  }
+
   /* useEffect(() => {
     interval.current = setInterval(async () => {
       try {
@@ -43,13 +64,13 @@ const UserList = () => {
   return (
     <div className="list-group">
       {users.map((user: User, index: number) => (
-        <UserItem key={index} user={user} />
+        <UserItem key={index} user={user} badge={showBadge(user.active_test_score)} />
       ))}
     </div>
   );
 };
 
-const UserItem = memo(({ user }: { user: User }) => (
+const UserItem = memo(({ user, badge }: { user: User, badge:JSX.Element }) => (
   <div className="list-group-item list-group-item-action d-flex align-items-center cursor-pointer">
     {/* <img src="../../assets/img/avatars/2.png" alt="User Image" className="rounded-circle me-3 w-px-50" /> */}
     <div className="avatar avatar-md me-2">
@@ -64,7 +85,7 @@ const UserItem = memo(({ user }: { user: User }) => (
           <small className="ms-1">{user.is_online ? "آنلاین" : "آفلاین"}</small>
         </div>
         <div className={`add-btn d-flex flex-column align-items-center ${styles.score}`}>
-          <i className="bx bx-award h3 mb-0"></i>
+          {badge}
           <span>{_(user.active_test_score.toString())}</span>
         </div>
       </div>
